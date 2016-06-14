b16cd385dfdb2043e029132344160581b37a4188
JYM <duddud311@naver.com>
2016-06-15 02:32:14 +0900

    ----------2.8.3V------------ DragAndDrop
      commited by YMM

--- mobile/src/main/java/com/knucapstone/tripjuvo/activity/DragAndDropTravelActivity.java
+++ mobile/src/main/java/com/knucapstone/tripjuvo/activity/DragAndDropTravelActivity.java
@@ -1,9 +1,17 @@
 package com.knucapstone.tripjuvo.activity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -12,16 +20,30 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.knucapstone.tripjuvo.R;
 import com.knucapstone.tripjuvo.adapter.DragAndDropTravelAdapter;
+import com.knucapstone.tripjuvo.database.model.PoiModel;
+import com.knucapstone.tripjuvo.fragment.PoiListFragment;
+import com.knucapstone.tripjuvo.geolocation.Geolocation;
+import com.knucapstone.tripjuvo.geolocation.GeolocationListener;
 import com.knucapstone.tripjuvo.model.DummyModel;
 import com.knucapstone.tripjuvo.model.TravelDummyModel;
+import com.knucapstone.tripjuvo.utility.ImageLoaderUtility;
+import com.knucapstone.tripjuvo.utility.LocationUtility;
+import com.knucapstone.tripjuvo.utility.Logcat;
+import com.knucapstone.tripjuvo.utility.PermissionUtility;
 import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
 import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.TouchViewDraggableManager;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
+import java.util.Date;
 
-public class DragAndDropTravelActivity extends AppCompatActivity {
+public class DragAndDropTravelActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
 	private DynamicListView mDynamicListView;
 	Intent intent;
@@ -29,11 +51,27 @@ public class DragAndDropTravelActivity extends AppCompatActivity {
 	ArrayList<Location> locationArrayList;
 	ArrayList<String> pictureArrayList;
 
+	private LatLng curPoint;
+	private double curLati = 0;
+	private double curLongi = 0;
+	private GoogleApiClient mGoogleApiClient;
+	private Location mLastLocation;
+
 	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_views);
+
+		//getCurrentPostion();
+		// Create an instance of GoogleAPIClient.
+		if (mGoogleApiClient == null) {
+			mGoogleApiClient = new GoogleApiClient.Builder(this)
+					.addConnectionCallbacks(this)
+					.addOnConnectionFailedListener(this)
+					.addApi(LocationServices.API)
+					.build();
+		}
 
 		mDynamicListView = (DynamicListView) findViewById(R.id.dynamic_listview);
 		mDynamicListView.setDividerHeight(0);
@@ -43,12 +81,40 @@ public class DragAndDropTravelActivity extends AppCompatActivity {
 		checkedItemsArrayList = intent.getStringArrayListExtra("CHECKED_ITEMS");
 		list = new ArrayList<>();
 
+//		synchronized (this)
+//		{
+//			try {
+//				this.wait();
+//			}
+//			catch (Exception e)
+//			{
+//
+//			}
+//		}
 		setUpDragAndDrop();
 		Toast.makeText(this, "Long press an item to start dragging",
 				Toast.LENGTH_SHORT).show();
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setTitle("여행 추천 경로");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+	}
+
+	@Override
+	protected void onStart() {
+		mGoogleApiClient.connect();
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		mGoogleApiClient.disconnect();
+		super.onStop();
 	}
 
 	@Override
@@ -71,9 +137,16 @@ public class DragAndDropTravelActivity extends AppCompatActivity {
 		}
 		ArrayList<TravelDummyModel> list = new ArrayList<>();
 
+		curPoint = new LatLng(curLati, curLongi);
+		Log.i("curPoint", curPoint.latitude+" @@ " + curPoint.longitude);
+
 		for(int i = 0; i < locationArrayList.size(); i ++)
 		{
-			list.add(new TravelDummyModel(i, pictureArrayList.get(i), locationArrayList.get(i).getProvider(), R.string.fontello_heart_empty,"10m","100"));
+			list.add(new TravelDummyModel(i, pictureArrayList.get(i),
+											locationArrayList.get(i).getProvider(),
+											R.string.fontello_heart_empty,
+											calculatePoiDistances(i)+"",
+											"100"));
 		}
 
 		//final
@@ -112,4 +185,81 @@ public class DragAndDropTravelActivity extends AppCompatActivity {
 			}
 		});
 	}
+
+	private int calculatePoiDistances(int index)
+	{
+		int distance = 0;
+//		if(curPoint!=null)
+//		{
+//			for(int i=0; i<locationArrayList.size(); i++)
+//			{
+//				LatLng cityLocation = new LatLng(locationArrayList.get(index).getLatitude(), locationArrayList.get(index).getLongitude());
+//				Log.i("!@#$", locationArrayList.get(index).getLatitude() +"");
+//				distance = LocationUtility.getDistance(curPoint, cityLocation);
+//				Log.i("distance", distance+"");
+//			}
+//		}
+		LatLng cityLocation = new LatLng(locationArrayList.get(index).getLatitude(), locationArrayList.get(index).getLongitude());
+		Log.i("!@#$", locationArrayList.get(index).getLatitude() +"");
+		distance = LocationUtility.getDistance(curPoint, cityLocation);
+		Log.i("distance", distance+"");
+
+		return distance;
+	}
+
+	public void getCurrentPostion()
+	{
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				//showNewLocation(location);
+				//curPoint = new LatLng(location.getLatitude(), location.getLongitude());
+				curLati = location.getLatitude();
+				curLongi = location.getLongitude();
+				String msg = "Lati : " + curLati + "\nLongi : "+ curLongi;
+				Log.i("myLocation", msg);
+			}
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+			}
+			public void onProviderEnabled(String provider) {
+			}
+			public void onProviderDisabled(String provider) {
+			}
+		} ;
+
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+		if (locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER))
+			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+		if (locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER))
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+				mGoogleApiClient);
+		if (mLastLocation != null) {
+//			mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+//			mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+			curLati = mLastLocation.getLatitude();
+			curLongi = mLastLocation.getLongitude();
+			Log.i("onConnected", "Lati : " + curLati + "@@" + "Longi : " + curLongi);
+			curPoint = new LatLng(curLati, curLongi);
+			Log.i("onConnected", curPoint.latitude+" @@ " + curPoint.longitude);
+		}
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+
+	}
+
 }
